2d7dfbb50c80ef375ef3ac3a50df2058b35b91ee
a9na <anaiscoding@gmail.com>
2024-07-29 18:46:49 +0200

    commit

---
+++ index.js
@@ -0,0 +1,20 @@
+import React from "react"
+import reactDOM from "react-dom"
+
+/*
+const page = (
+    <div>
+        <img src="./react-logo.png" width="400 px"/>
+        <h1>Fun facts about React</h1>
+        <ul>
+            <li>Was first released in 2013</li>
+            <li>Was originally created by Jordan Walke</li>
+            <li>Has well over 100K stars on GitHub</li>
+            <li>Is maintained by Facebook</li>
+            <li>Powers thousands of enterprises</li>
+        </ul>
+    </div>
+)
+*/
+
+ReactDOM.render(page, document.getElementById("root"))
